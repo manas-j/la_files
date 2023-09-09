be5166e807d36b331e6813c339824db6106ad509
Manas Jha <105232596+manas-j@users.noreply.github.com>
2023-09-09 17:33:17 +0000

    Add files via upload

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
 import pyodbc
 import pymongo
 import faiss
@@ -18,7 +24,16 @@ from bson.json_util import loads, dumps
 from azure.storage.queue import QueueServiceClient, QueueClient, QueueMessage
 app = FastAPI()
 
+
+# In[2]:
+
+
 origins = ["*"]
+
+
+# In[3]:
+
+
 app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
@@ -26,6 +41,12 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
+
+# In[4]:
+
+
+import pyodbc
 
 server = 'sql-fullstack-sandbox-ci-001.database.windows.net'
 database = 'lvlaccess_embeddings'
@@ -35,6 +56,10 @@ driver= 'ODBC Driver 17 for SQL Server;'
 
 cnxn = pyodbc.connect('DRIVER='+driver+';PORT=1433;SERVER='+server+';PORT=1443;DATABASE='+database+';UID='+username+';PWD='+ password)
 cursor = cnxn.cursor()
+
+
+# In[5]:
+
 
 def get_knowledge_base():
     sql_select = "SELECT * FROM EMBEDDINGS_STORAGE_levelaccess"
@@ -57,9 +82,21 @@ def get_knowledge_base():
     
     return txt_lst, embd_lst, meta_lst
 
+
+# In[6]:
+
+
 txt, embd, meta = get_knowledge_base()
 
-openai.api_key = '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'
+
+# In[98]:
+
+
+openai.api_key = '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'
+
+
+# In[8]:
+
 
 def num_tokens_from_string(string: str, encoding_name: str) -> int:
     """Returns the number of tokens in a text string."""
@@ -67,14 +104,36 @@ def num_tokens_from_string(string: str, encoding_name: str) -> int:
     num_tokens = len(encoding.encode(string))
     return num_tokens
 
+
+# In[9]:
+
+
 index = faiss.IndexFlatL2(len(embd[0]))
 index.add(np.array(embd))
 
+
+# In[91]:
+
+
 def generate_indices(txt_lst,meta_data,embd_lst,ques,index):
-    response = openai.Embedding.create(
-      model = 'text-embedding-ada-002',
-        input = [ques]
-    )
+    try: 
+        response = openai.Embedding.create(
+          model = 'text-embedding-ada-002',
+            input = [ques]
+        )
+    except openai.error.APIError as e:
+      #Handle API error here, e.g. retry or log
+      print(f"OpenAI API returned an API Error: {e}")
+      pass
+    except openai.error.APIConnectionError as e:
+      #Handle connection error here
+      print(f"Failed to connect to OpenAI API: {e}")
+      pass
+    except openai.error.RateLimitError as e:
+      #Handle rate limit error (we recommend using exponential backoff)
+      print(f"OpenAI API request exceeded rate limit: {e}")
+      pass
+
     txt_embd = response['data'][0]['embedding']
     dist, ind = index.search(np.array(txt_embd).reshape(1,-1), 10)
     txts = []
@@ -83,34 +142,71 @@ def generate_indices(txt_lst,meta_data,embd_lst,ques,index):
 
     return txts,ind
 
+
+# In[90]:
+
+
 def gen_ques(mbody):
     prompt = 'From the given email body, extract the questions that are asked by the sender to the receiver. Make sure that the questions generated follow the email wording strictly and are returned in the form of a list without an index.'
     prompt += 'Email body: """' + mbody + '"""'
-    response = openai.Completion.create(
-    model='text-davinci-003', 
-    prompt=prompt, 
-    max_tokens=256,
-    temperature=0.3,
-    stop=[' END'])
+    
+    try:
+        response = openai.Completion.create(
+        model='text-davinci-003', 
+        prompt=prompt, 
+        max_tokens=256,
+        temperature=0.3,
+        stop=[' END'])
+
+    except openai.error.APIError as e:
+      #Handle API error here, e.g. retry or log
+      print(f"OpenAI API returned an API Error: {e}")
+      pass
+    except openai.error.APIConnectionError as e:
+      #Handle connection error here
+      print(f"Failed to connect to OpenAI API: {e}")
+      pass
+    except openai.error.RateLimitError as e:
+      #Handle rate limit error (we recommend using exponential backoff)
+      print(f"OpenAI API request exceeded rate limit: {e}")
+      pass
+
     mbody_ext =  response["choices"][0]["text"].strip()
     mbody_ext = mbody_ext.strip("[]")
     ls = [i.strip("''") for i in mbody_ext.split(", ")]
     return ls
 
-def return_opt_prov(ques,predt,embd,idcs,meta):
+
+# In[105]:
+
+
+def return_opt_prov(ques,predt,embd,idcs,meta,instruct):
     embd = np.array(embd)
     prompt = 'Context : """'
     for i in predt:
         prompt+=i+"\n"
     prompt += '"""'
     prompt += instruct + 'Question: """' + ques + '"""'
-
-    response = openai.Completion.create(
-        model='text-davinci-003', 
-        prompt=prompt, 
-        max_tokens=256,
-        temperature=0.3,
-        stop=[' END'])
+    
+    try:
+        response = openai.Completion.create(
+            model='text-davinci-003', 
+            prompt=prompt, 
+            max_tokens=256,
+            temperature=0.3,
+            stop=[' END'])
+    except openai.error.APIError as e:
+      #Handle API error here, e.g. retry or log
+      print(f"OpenAI API returned an API Error: {e}")
+      pass
+    except openai.error.APIConnectionError as e:
+      #Handle connection error here
+      print(f"Failed to connect to OpenAI API: {e}")
+      pass
+    except openai.error.RateLimitError as e:
+      #Handle rate limit error (we recommend using exponential backoff)
+      print(f"OpenAI API request exceeded rate limit: {e}")
+      pass
 
     predt = response["choices"][0]["text"].strip()
     atb = predt.replace("\n",'').split('.')
@@ -166,57 +262,89 @@ def return_opt_prov(ques,predt,embd,idcs,meta):
     
     return predts, indpt, stn, score
 
+
+# In[81]:
+
+
 CONNECTION_STRING = 'mongodb+srv://FullStackAdmin:Hello%4012345@mongo-rfp-sandbox-eus-001.mongocluster.cosmos.azure.com/?tls=true&authMechanism=SCRAM-SHA-256&retrywrites=false&maxIdleTimeMS=120000'
 DB_NAME = "foundationalai"
-COLLECTION_NAME = "documents"
+COLLECTION_NAME = "usersEmail"
 # Create database if it doesn't
 client = pymongo.MongoClient(CONNECTION_STRING)
 db = client[DB_NAME]
 collection = db[COLLECTION_NAME]
-docs = db.documents
 
-def fetch_mongoID():
+
+# In[73]:
+
+
+def fetch_messageID():
     queue_url = 'https://sam1cin001.queue.core.windows.net/queue-mail-m1-cin-001'
     queue_name = 'queue-mail-m1-cin-001'
     SaS_credential = '?sv=2022-11-02&ss=bq&srt=sco&sp=rwdlacupiytfx&se=2023-10-03T20:50:52Z&st=2023-09-04T12:50:52Z&spr=https,http&sig=8dQCWp1dWB%2B1FJrqbff9DZBIzVE8h2nBFLaRcxdNyPY%3D'
     connection_string = 'BlobEndpoint=https://sam1cin001.blob.core.windows.net/;QueueEndpoint=https://sam1cin001.queue.core.windows.net/;FileEndpoint=https://sam1cin001.file.core.windows.net/;TableEndpoint=https://sam1cin001.table.core.windows.net/;SharedAccessSignature=' + SaS_credential
     queue_client = QueueClient.from_connection_string(connection_string, queue_name, SaS_credential)
+    message = queue_client.peek_messages()[0]
     if(len(message)>0):
-        message = queue_client.peek_messages()[0]
         return message["id"]
     if(len(message)==0):
         return None
-    
-def prcs(MongoID, text_lst, meta_lst, embd_lst, index):
-    if MongoID == None:
+
+
+# In[106]:
+
+
+def prcs(msgID, text_lst, meta_lst, embd_lst, index):
+    if msgID == None:
         return None
-    email_bson = docs.find_one({"_id":MongoID})
-    filter = {"_id": MongoID}
+    email_bson = collection.find_one({"MessageId":msgID})
+    if email_bson == None:
+        return None
+    filter = {"MessageId": msgID}
     #email_json = json.loads(email_bson)
-    ans_ext_id = []
-    for i in range(len(email_bson["Mail"]["Message"])):
-        m_body = str(email_bson["Mail"]["Message"][i]["MailBody"])
-        ls = gen_ques(m_body)
+    snip = email_bson["Snippet"]
+    ques = gen_ques(snip)
+    for key in ques:
         ques_indcs = []
         indcs = []
         dic = {}
-        for j in ls:
-            instruct = 'You are a sales employee at LevelAccess. From the above given context answer the below question from a potential customer to the best of your knowledge. The answer should strictly be from the context and if you dont know or are unsure about it then just say "I Dont Know". Only refer to levelaccess when you answer the question and not Essential Accessibility. Assume that by "you" or "your" in the question, they mean LevelAccess. Also pay special attention to any corporation names that are mentioned and only include them in the answer if they are present in the contect. Follow the question wording strictly and donot use synonyms. Donot mention "Answer" in the beginning of the response.\n'
-            text, ind = generate_indices(text_lst,meta_lst,embd_lst,j,index)
-            predts, indpt, conf, score = return_opt_prov(j,text,embd_lst,ind,meta_lst)
-            ref = ''
-            for k in indpt:
-                ref += k
-            #print(ref)
-            dic[j] = {"Answer": predts[0], "ConfidenceScore": conf, "References": ref}
+        instruct = 'You are a sales employee at LevelAccess. From the above given context answer the below question from a potential customer to the best of your knowledge. The answer should strictly be from the context and if you dont know or are unsure about it then just say "I Dont Know". Only refer to levelaccess when you answer the question and not Essential Accessibility. Assume that by "you" or "your" in the question, they mean LevelAccess. Also pay special attention to any corporation names that are mentioned and only include them in the answer if they are present in the contect. Follow the question wording strictly and donot use synonyms. Donot mention "Answer" in the beginning of the response.\n'
+        text, ind = generate_indices(text_lst,meta_lst,embd_lst,key,index)
+        predts, indpt, conf, score = return_opt_prov(key,text,embd_lst,ind,meta_lst,instruct)
+        ref = ''
+        for k in indpt:
+            ref += k
+        #print(ref)
+        out_string = predts[0] + "ConfidenceScore: " + conf + "References: "+ ref
+        dic[key] = {"Answer": out_string}
 
-        email_bson["Mail"]["Message"][i]["DictQnA"] = dic
-        email_new = email_bson
+    email_bson["emailsQuestions"] = dic
+    email_new = email_bson
     docs.update_one(filter,{"$set" : email_new})
     return email_new
 
+
+# In[110]:
+
+
 @app.get("/")
-async def root():
-    mongo_id = fetch_mongoID()
-    email_new = prcs(mongo_id, txt, meta, embd, index)
-    return None
+def fetch_mongoID():
+    msgId = fetch_messageID()
+    if mongoID:
+        return {"mongoID": mongoID}
+    else:
+        return {"message": "No message found in the queue"}
+    
+def process_email(
+    mongoID: str,
+    txt: list,
+    meta: list,
+    embd: list,
+    index: int
+):
+    email_result = prcs(msgId, txt, meta, embd, index)
+    if email_result:
+        return {"message": "Email processed successfully"}
+    else:
+        return {"message": "Email processing failed"}
+
